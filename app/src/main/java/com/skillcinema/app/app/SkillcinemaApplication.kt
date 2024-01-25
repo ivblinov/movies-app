4fe8bf7875bd932bd6e4a1fc15ee199eb88b1e1f
Ivan Blinov <iv2002blinov@gmail.com>
2024-01-25 08:43:20 +0700

    task:6 - добавил файл в папку scripts common_methods.gradle, удалил манифест из core-module

---
+++ app/src/main/java/com/skillcinema/app/app/SkillcinemaApplication.kt
@@ -0,0 +1,5 @@
+package com.skillcinema.app.app
+
+import android.app.Application
+
+internal class SkillcinemaApplication : Application()
---
+++ celebrity/core/src/main/java/com/skillcinema/celebrity/core/app/App.kt
@@ -0,0 +1,3 @@
+package com.skillcinema.celebrity.core.app
+
+interface App
