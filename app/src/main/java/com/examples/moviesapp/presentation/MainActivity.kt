b131667bfa31f8fad66ca779da603167942a7a93
Ivan Blinov <iv2002blinov@gmail.com>
2024-06-23 20:15:36 +0700

    task: 1.2 - Подключил AndroidX Navigation, task: 1.3 - Добавил и подключил Bottom Navigation.

--- app/src/main/java/com/examples/moviesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/examples/moviesapp/presentation/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.examples.moviesapp.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.examples.moviesapp.R
+import com.examples.moviesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.idBottomNav.setupWithNavController(navHostFragment.navController)
     }
 }
