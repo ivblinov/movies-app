99cd359554f2ff556e98bf996ac04f313fff79c9
Ivan Blinov <iv2002blinov@gmail.com>
2024-06-23 14:24:07 +0700

    task:1 - Разбил приложение на слои в соответствие с Clean Architecture.

--- app/src/main/java/com/examples/moviesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/examples/moviesapp/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.examples.moviesapp
+package com.examples.moviesapp.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.examples.moviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
